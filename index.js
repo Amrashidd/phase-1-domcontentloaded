291221f5493325ab10eb500fb443225458a8ecba
ahmed rashid <ahmeddmrashid@gmail.com>
2022-08-09 21:04:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+const master = document.querySelector("#text")
+ master.innerHTML = "This is really cool!";
+
+})
